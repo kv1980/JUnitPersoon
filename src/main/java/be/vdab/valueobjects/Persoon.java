1e93065ee22b8ae41c61bd435dba2394a909443e
Kristof Vermeulen <kristofvermeulen1980@gmail.com>
2018-03-16 14:44:02 +0100

    eerste versie oplossing JUnit

---
+++ src/main/java/be/vdab/valueobjects/Persoon.java
@@ -0,0 +1,54 @@
+package be.vdab.valueobjects;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Persoon {
+	private List<String> voornamen = new ArrayList<>();
+	
+	public Persoon(List<String> voornamen) {
+		if (voornamen.isEmpty()) {
+			throw new IllegalArgumentException("Foutmelding: de voornamenlijst is leeg.");
+		} 
+		if (valideer(voornamen)) {
+			this.voornamen = voornamen;
+		}
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder zin = new StringBuilder();
+		for (String voornaam : voornamen) {
+			zin.append(" ");
+			zin.append(voornaam);
+		}
+		return zin.substring(1).toString();
+	}
+	
+	private boolean valideer(List<String> voornamen) {
+		boolean eenVoornaamIsLeeg = false;
+		boolean eenVoornaamBevatEenBlancoTeken = false;	
+		boolean eenVoornaamKomtMeerdereKerenVoor = false;	
+		for (String voornaam : voornamen) {
+			if (voornaam.isEmpty()) {
+				eenVoornaamIsLeeg = true;
+			}
+			if (voornaam.contains(" ")) {
+				eenVoornaamBevatEenBlancoTeken = true;
+			}
+			if (voornamen.indexOf(voornaam)!=voornamen.lastIndexOf(voornaam)) {
+				eenVoornaamKomtMeerdereKerenVoor = true;
+			}
+		}
+		if (eenVoornaamIsLeeg) {
+			throw new IllegalArgumentException("Foutmelding: minstens een van de voornamen is leeg.");
+		}
+		if (eenVoornaamBevatEenBlancoTeken) {
+			throw new IllegalArgumentException("Foutmelding: minstens een van de voornamen is bevat een blanco teken.");
+		}
+		if (eenVoornaamKomtMeerdereKerenVoor) {
+			throw new IllegalArgumentException("Foutmelding: minstens een van de voornamen komt meerdere keren voor.");
+		}
+		return !eenVoornaamIsLeeg && !eenVoornaamBevatEenBlancoTeken && !eenVoornaamKomtMeerdereKerenVoor;
+	}
+}
---
+++ src/test/java/be/vdab/valueobjects/PersoonTest.java
@@ -0,0 +1,72 @@
+package be.vdab.valueobjects;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PersoonTest {
+	private List<String> voornamen;
+
+	@Before
+	public void before() {
+		voornamen = new ArrayList<>();
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void een_persoon_zonder_één_voornaam_bestaat_niet() {
+		Persoon persoon = new Persoon(voornamen);
+	}
+
+	@Test
+	public void een_persoon_kan_één_voornaam_hebben() {
+		voornamen.add("Hans");
+		Persoon persoon = new Persoon(voornamen);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void een_persoon_kan_geen_twee_gelijke_voornamen_hebben() {
+		voornamen.add("Hans");
+		voornamen.add("Hans");
+		Persoon persoon = new Persoon(voornamen);
+	}
+
+	@Test
+	public void een_persoon_kan_wel_meerdere_verschillende_voornamen_hebben() {
+		voornamen.add("Hans");
+		voornamen.add("Kristof");
+		Persoon persoon = new Persoon(voornamen);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void geen_enkele_voornaam_mag_leeg_zijn() {
+		voornamen.add("Hans");
+		voornamen.add("");
+		Persoon persoon = new Persoon(voornamen);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void geen_enkele_voornaam_mag_een_blanco_teken_bevatten() {
+		voornamen.add("Hans");
+		voornamen.add("Kristof ");
+		Persoon persoon = new Persoon(voornamen);
+	}
+
+	@Test
+	public void toString_geeft_String_met_meerdere_voornamen_gescheiden_door_een_spatie() {
+		voornamen.add("Hans");
+		voornamen.add("Kristof");
+		Persoon persoon = new Persoon(voornamen);
+		assertEquals("Hans Kristof", persoon.toString());
+	}
+
+	@Test
+	public void toString_geeft_String_met_enige_voornaam() {
+		voornamen.add("Hans");
+		Persoon persoon = new Persoon(voornamen);
+		assertEquals("Hans", persoon.toString());
+	}
+}
